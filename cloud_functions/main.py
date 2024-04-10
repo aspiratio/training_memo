951831caa698655d7da34b39211486cdfebf5d57
YUHEI <80562226+aspiratio@users.noreply.github.com>
2024-04-10 23:09:55 +0900

    Firestoreへの疎通確認まで

--- cloud_functions/main.py
+++ cloud_functions/main.py
@@ -1,6 +1,27 @@
-def my_function(request):
-    params = request.args
-    if "name" in params:
-        return f"Hello, {params['name']}!"
-    else:
-        return "Hello, World!"
+from google.cloud import firestore
+import os
+from dotenv import load_dotenv
+
+# .env ファイルから環境変数をロード
+load_dotenv()
+
+# 最上流のコレクション名
+root_collection_name = os.getenv("FIRESTORE_ROOT_COLLECTION")
+# 最上流配下のドキュメントID（環境毎にDBを変える場合はここで分ける）
+root_doc_id = os.getenv("FIRESTORE_ROOT_DOCUMENT")
+
+# Firestoreに接続
+db = firestore.Client()
+root_doc = db.collection(root_collection_name).document(root_doc_id)
+
+
+def set_documents(collection_name: str, data: dict):
+    try:
+        print(f"{collection_name}にドキュメントの登録が成功しました")
+
+    except Exception:
+        raise
+
+
+def main(request):
+    set_documents("menu", {"test": "test"})
