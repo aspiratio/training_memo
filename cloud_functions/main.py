ef336e3d25458811cba5bfe7b465087505a466e2
YUHEI <80562226+aspiratio@users.noreply.github.com>
2024-04-12 23:59:26 +0900

    データの登録メソッドを作成

--- cloud_functions/main.py
+++ cloud_functions/main.py
@@ -31,48 +31,37 @@ def get_documents(collection_name: str, field: str, value) -> list:
     return docs
 
 
-def set_documents(collection_name: str, data: dict, doc_id: str = None):
+def add_document(collection_name: str, data: dict):
     collection_ref = root_doc.collection(collection_name)
-    if doc_id is None:
-        collection_ref.document().add(data)
-    else:
-        collection_ref.document().set(data)
+    update_time, doc_ref = collection_ref.add(data)
+    logger.info(f"{collection_name}にデータを登録しました")
+    return doc_ref
 
 
 def main(request):
     # クエリパラメータのチェック
     if request.args.get("walk") is None:
-        logger.error("クエリパラメータに walk がありません")
-        return "walk is required"
+        logger.info("歩数情報がありません")
+    else:
+        # Firestoreから歩数の menu_id を持つドキュメントを取得する
+        menu_docs = list(get_documents("menu", "name", "歩数"))
+        if len(menu_docs) == 0:
+            logger.error("menu に歩数が登録されていません")
+            return "menu is not found"
+        elif len(menu_docs) > 1:
+            logger.error("menu に複数の歩数が登録されています")
+            return "menu is not found"
 
-    # Firestoreから歩数の menu_id を持つドキュメントを取得する
-    menu_docs = get_documents("menu", "name", "歩数")
-    if menu_docs.length == 0:
-        logger.error("menu に歩数が登録されていません")
-        return "menu is not found"
-    elif menu_docs.length > 1:
-        logger.error("menu に複数の歩数が登録されています")
-        return "menu is not found"
+        menu_doc = menu_docs[0]
+        menu_id = menu_doc.id
+        walk_count = int(request.args.get("walk"))
 
-    menu_doc = menu_docs[0]
-    menu_id = menu_doc.to_dict()["id"]
-    walk_count = int(request.args.get("walk"))
+        data = {
+            "count": walk_count,
+            "menu_id": menu_id,
+            "created_at": firestore.SERVER_TIMESTAMP,
+            "updated_at": firestore.SERVER_TIMESTAMP,
+        }
 
-    # 当日の歩数情報があれば取得する
-    daily_record_docs = get_documents("daily_record", "menu_id", menu_id)
-    if daily_record_docs.length == 0:
-        print("新規登録の処理")
-    elif daily_record_docs.length == 1:
-        print(
-            "既存の歩数に足した歩数を登録する処理（必要か？SELECTするときに足すほうがいい？）"
-        )
-    elif daily_record_docs.length > 1:
-        logger.error("daily_record に複数の当日の歩数情報が登録されています")
-        return "daily_record is not found"
-
-    # data = {
-    #     "count": walk_count,
-    #     "menu_id": menu_id,
-    #     "created_at": firestore.SERVER_TIMESTAMP,
-    #     "updated_at": firestore.SERVER_TIMESTAMP,
-    # }
+        add_document("daily_record", data)
+        return {"status": 200}
