d41dae6a35e90157420770e47c327a5ac3cce944
YUHEI <80562226+aspiratio@users.noreply.github.com>
2024-04-13 15:44:49 +0900

    データ登録処理の修正とログ出力の改良

--- cloud_functions/main.py
+++ cloud_functions/main.py
@@ -1,5 +1,5 @@
 import os
-from logging import DEBUG, StreamHandler, getLogger
+from logging import DEBUG, Formatter, StreamHandler, getLogger
 from google.cloud import firestore
 from google.cloud.firestore_v1.base_query import FieldFilter
 from dotenv import load_dotenv
@@ -12,6 +12,8 @@ logger = getLogger(__name__)
 handler = StreamHandler()
 handler.setLevel(DEBUG)
 logger.setLevel(DEBUG)
+formatter = Formatter("[%(filename)s:%(lineno)d] %(message)s")
+handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
@@ -29,21 +31,21 @@ def main(request):
     # リクエスト情報を取得する
     request_path = request.path
     request_body = request.json
+    print(request_body)
 
     # パスパラメータごとに処理を分ける
     if request_path == "/daily_record":
         try:
             add_daily_record(request_body)
         except Exception as e:
-            logger.error(e)
-            return {"status": 500, "message": str(e)}
+            logger.error(e, exc_info=True)
+            return {"status": 500, "message": "記録に失敗しました"}
     elif request_path == "/menu":
         try:
-            # トレーニング目標を記録する処理
-            print("")
+            set_training_menu(request_body)
         except Exception as e:
-            logger.error(e)
-            return {"status": 500, "message": e}
+            logger.error(e, exc_info=True)
+            return {"status": 500, "message": "設定に失敗しました"}
     else:
         logger.info("無効なパスパラメータでリクエストされました")
         return {"status": 404}
@@ -53,7 +55,7 @@ def main(request):
 
 def add_daily_record(request_body: dict):
     """
-    歩数の記録を追加する
+    トレーニング記録を追加する
 
     Parameters:
     - request_body (dict): リクエストボディ
@@ -64,9 +66,7 @@ def add_daily_record(request_body: dict):
     - Exception: メニューが登録されていない場合
     - Exception: 複数のメニューが登録されている場合
     """
-    menu_docs = list(
-        get_documents("menu", "name", request_body["menu"])
-    )  # Firestoreから歩数の menu_id を持つドキュメントを取得する
+    menu_docs = list(get_documents("menu", "name", request_body["menu"]))
     if len(menu_docs) == 0:
         raise Exception("menu が登録されていません")
     elif len(menu_docs) > 1:
@@ -80,13 +80,42 @@ def add_daily_record(request_body: dict):
         "updated_at": firestore.SERVER_TIMESTAMP,
     }
 
-    add_document("daily_record", data)
+    set_document("daily_record", data)
+
+
+def set_training_menu(request_body: dict):
+    """
+    トレーニングノルマを設定する
+
+    Parameters:
+    - request_body (dict): リクエストボディ
+        - name (str): トレーニングメニュー
+        - unit (str): 単位（分や回など）
+        - weekly_quota (int): 週あたりのノルマ
+
+    Raises:
+    - Exception: 複数のメニューが登録されている場合
+    """
+    menu_docs = list(get_documents("menu", "name", request_body["name"]))
+    if len(menu_docs) > 1:
+        raise Exception("menu が複数登録されています")
+
+    menu_id = menu_docs[0].id
+    data = {
+        "name": request_body["name"],
+        "unit": request_body["unit"],
+        "weekly_quota": request_body["weekly_quota"],
+        "created_at": firestore.SERVER_TIMESTAMP,
+        "updated_at": firestore.SERVER_TIMESTAMP,
+    }
+
+    set_document("menu", data, menu_id)
 
 
 # Firestoreへのリクエスト
 def get_documents(collection_name: str, field: str, value):
     """
-    Firestoreからドキュメントを取得する
+    Firestoreから指定した検索条件に一致するドキュメントを取得する
 
     Parameters:
     - collection_name (str): コレクション名
@@ -101,18 +130,26 @@ def get_documents(collection_name: str, field: str, value):
     return docs
 
 
-def add_document(collection_name: str, data: dict):
+def set_document(collection_name: str, data: dict, doc_id: str = ""):
     """
-    Firestoreにドキュメントを追加する
+    Firestoreにドキュメントを追加する。doc_idを指定すれば上書きする
 
     Parameters:
     - collection_name (str): コレクション名
     - data (dict): 追加するデータ
+    - doc_id (str): 上書きするドキュメントID
 
     Returns:
     - doc_ref: 追加したドキュメントの参照
     """
     collection_ref = root_doc.collection(collection_name)
-    update_time, doc_ref = collection_ref.add(data)
+    if doc_id == "":
+        doc_ref = collection_ref.document().set(
+            data
+        )  # 自動生成したドキュメントIDで登録する
+    else:
+        doc_ref = collection_ref.document(doc_id).set(
+            data
+        )  # 指定されたドキュメントに上書きする
     logger.info(f"{collection_name}にデータを登録しました")
     return doc_ref
